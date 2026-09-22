cd167e663eba8f978de6c2b120a8440b0087dbb7
Dan Do <dando985@gmail.com>
2026-09-22 17:11:39 -0500

    moved configuration properties scan to main applcation

--- src/main/java/com/dando/pacepilot/PacePilotApplication.java
+++ src/main/java/com/dando/pacepilot/PacePilotApplication.java
@@ -1,9 +1,12 @@
 package com.dando.pacepilot;
 
+import com.dando.pacepilot.embedding.ollama.OllamaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OllamaProperties.class)
 public class PacePilotApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dando/pacepilot/embedding/config/EmbeddingConfiguration.java
+++ src/main/java/com/dando/pacepilot/embedding/config/EmbeddingConfiguration.java
@@ -3,13 +3,11 @@ package com.dando.pacepilot.embedding.config;
 import com.dando.pacepilot.embedding.EmbeddingProvider;
 import com.dando.pacepilot.embedding.ollama.OllamaEmbeddingProvider;
 import com.dando.pacepilot.embedding.ollama.OllamaProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@EnableConfigurationProperties(OllamaProperties.class)
 public class EmbeddingConfiguration {
 
     @Bean
