f7e8f88e0fd43f1b50ea63afee3a01e70320435d
Dan Do <dando985@gmail.com>
2026-09-23 12:35:53 -0500

    moved file location to dedicated config folder

---
+++ src/main/java/com/dando/pacepilot/config/OllamaConfiguration.java
@@ -0,0 +1,32 @@
+package com.dando.pacepilot.config;
+
+import com.dando.pacepilot.embedding.EmbeddingProvider;
+import com.dando.pacepilot.embedding.ollama.OllamaEmbeddingProvider;
+import com.dando.pacepilot.generation.TextGenerationProvider;
+import com.dando.pacepilot.generation.ollama.OllamaTextGenerationProvider;
+import com.dando.pacepilot.ollama.OllamaProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+@EnableConfigurationProperties(OllamaProperties.class)
+public class OllamaConfiguration {
+
+    @Bean
+    public RestClient ollamaRestClient(OllamaProperties properties) {
+        return RestClient.builder().baseUrl(properties.baseUrl().toString()).build();
+    }
+
+    @Bean
+    public EmbeddingProvider embeddingProvider(@Qualifier("ollamaRestClient") RestClient restClient, OllamaProperties properties) {
+        return new OllamaEmbeddingProvider(restClient, properties.embeddingModel());
+    }
+
+    @Bean
+    public TextGenerationProvider textGenerationProvider(@Qualifier("ollamaRestClient") RestClient restClient, OllamaProperties properties) {
+        return new OllamaTextGenerationProvider(restClient, properties.chatModel());
+    }
+}
