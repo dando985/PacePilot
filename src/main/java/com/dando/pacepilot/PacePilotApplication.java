6f2e14782fc716ac76fb6d4b625e4800778897c4
Dan Do <dando985@gmail.com>
2026-09-23 17:19:57 -0500

    removed unnecessary configuration properties

--- src/main/java/com/dando/pacepilot/PacePilotApplication.java
+++ src/main/java/com/dando/pacepilot/PacePilotApplication.java
@@ -1,12 +1,9 @@
 package com.dando.pacepilot;
 
-import com.dando.pacepilot.ollama.OllamaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(OllamaProperties.class)
 public class PacePilotApplication {
 
     public static void main(String[] args) {
